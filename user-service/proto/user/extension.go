a45d5cf91fd50093ac8df8af3a9289669703dd96
hackerrithm <kemargalloway@gmail.com>
2018-08-04 17:39:59 -0500

    update remote repo with corrections

--- user-service/proto/user/extension.go
+++ user-service/proto/user/extension.go
@@ -1,11 +1,17 @@
 package go_micro_srv_user
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	"github.com/satori/go.uuid"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	uuid, _ := uuid.NewV4()
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		return err
+	}
 	return scope.SetColumn("Id", uuid.String())
 }
