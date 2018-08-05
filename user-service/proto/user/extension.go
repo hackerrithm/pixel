816ac2832e98db9c78998a0ad767be87bcdd2ca2
hackerrithm <kemargalloway@gmail.com>
2018-08-04 19:13:55 -0500

    update extension

--- user-service/proto/user/extension.go
+++ user-service/proto/user/extension.go
@@ -1,11 +1,17 @@
 package go_micro_srv_user
 
 import (
+	"fmt"
+
 	"github.com/jinzhu/gorm"
 	"github.com/satori/go.uuid"
 )
 
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	u1 := uuid.Must(uuid.NewV4())
+	u1, err := uuid.NewV4()
+	if err != nil {
+		fmt.Printf("Something went wrong: %s", err)
+		return err
+	}
 	return scope.SetColumn("Id", u1.String())
 }
