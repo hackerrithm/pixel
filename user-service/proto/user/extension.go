f29149259fa4972df7df713f23b50814d96d9aed
hackerrithm <kemargalloway@gmail.com>
2018-08-04 20:52:28 -0500

    update extension

--- user-service/proto/user/extension.go
+++ user-service/proto/user/extension.go
@@ -1,12 +1,12 @@
 package go_micro_srv_user
 
 import (
+	"github.com/google/uuid"
 	"github.com/jinzhu/gorm"
-	"github.com/satori/go.uuid"
 )
 
 // BeforeCreate does action before create
 func (model *User) BeforeCreate(scope *gorm.Scope) error {
-	u1 := uuid.Must(uuid.NewV4())
+	u1 := uuid.New()
 	return scope.SetColumn("Id", u1.String())
 }
