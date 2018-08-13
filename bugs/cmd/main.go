25cd639931d402fe16e5bba18f5c097a32b0dd98
hackerrithm <kemargalloway@gmail.com>
2018-08-12 21:04:18 -0500

    initial go-kit setup

---
+++ bugs/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import service "github.com/hackerrithm/pixel/bugs/cmd/service"
+
+func main() {
+	service.Run()
+}
---
+++ users/cmd/main.go
@@ -0,0 +1,7 @@
+package main
+
+import service "github.com/hackerrithm/pixel/users/cmd/service"
+
+func main() {
+	service.Run()
+}
